1458a7e82734a0815acca6193ade9efa68791156
arippicnic <arifdwimusic@gmail.com>
2022-03-13 16:03:50 +0700

    sesion name

--- server/routes/adminRouter.js
+++ server/routes/adminRouter.js
@@ -5,15 +5,13 @@ import responses from "&/utils/responses";
 
 const router = express.Router();
 
-const { ADMIN_SESSION } = process.env;
-
 router.post("/post", async (req, res) => {
   const created = await Posts.create({ ...req.body });
   return responses.OK(res, {});
 });
 
 router.delete("/signOut", async (req, res) => {
-  res.cookie(ADMIN_SESSION, "none", {
+  res.cookie("ADMIN_SESSION", "none", {
     expires: new Date(Date.now() + 5 * 1000),
     httpOnly: true,
   });
